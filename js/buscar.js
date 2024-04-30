c46f19a77df819d45cf98fe6ba5ff520f88bdae2
Rasmussen <bruna.rasmussen@accenture.com>
2024-04-29 21:23:27 -0300

    finalizacao projeto bootcamp

--- js/buscar.js
+++ js/buscar.js
@@ -9,11 +9,40 @@ async function buscar(){ //async é assincrona
     console.log("Resposta: ", respostaDadosGame); //exibe os dados da constante q ta pegando da url
 
 
-    const cards = document.getElementById("cards_games");
+    const cards = document.getElementById("cards_games"); //pega a div mãe onde os cards vão carregar
 
-    cards.innerHTML = respostaDadosGame.map((itemAgenda) => {
-            return "TESTE"
-    });// O  MAP é tipo um FOR. isso dentro do () é uma fun
+    cards.innerHTML = respostaDadosGame.map((itemAgenda) => { //o MAP faz a iteração e guarda como array, cada item do array é um itemAgenda
+            return `
+            <div class="cardItem">
+            <div class="dataGame"> 
+                <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}  
+            </div>
+            <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+            <div class="descricao">
+                <p> <strong>${itemAgenda.nome}</strong> </p>
+                <p> ${itemAgenda.descricao} </p>
+                <p><strong>Gamers: </strong></p>
+
+                <div class="gamers">
+
+                        ${itemAgenda.gamers.map((gamerItem)=>{
+                                return`
+                                <div class="gamerItem">@${gamerItem}</div>
+                                `
+                        }).join('')//tirar a virgula
+                } //função pra percorrer lista dentro da lista
+
+                </div>
+
+              
+            </div>  
+            <div class="assistir">
+                    <a class="assistirItem" href="${itemAgenda.urlAssistir} ">  <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+            </div>
+        </div>
+
+            `
+    }).join('');// O  MAP é tipo um FOR. isso dentro do () é uma fun
 
 }
 
